2c706e30d553a55063d80f6204b494f4894966dc
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-09-24 10:15:10 +0200

    [FEATURE][LOCALIZABLES] * Localizables files were added to use Spanish and English language

--- Increment/Commons/Constants/Landing/LandingConstants.swift
+++ Increment/Commons/Constants/Landing/LandingConstants.swift
@@ -3,10 +3,9 @@ import SwiftUI
 
 struct LandingConstants {
     static let landingBackgroundImageName = "ropes"
-    static let landingTitleText = "Increment"
+    static let landingTitleText = NSLocalizedString("landing_title_text", comment: "Landing title text")
     static let landingTitleSize: CGFloat = 64
     static let percentTitle: CGFloat = 0.15
     static let landingImageSystemButtonName = "plus.circle.fill"
-
-    
+    static let landingButtonCreateNewIncreaseTitleText = NSLocalizedString("landing_button_create_new_increase_text", comment: "Landing create new challenge button text")
 }
--- Increment/Landing/LandingView.swift
+++ Increment/Landing/LandingView.swift
@@ -1,45 +1,52 @@
 import SwiftUI
 
 struct LandingView: View {
+    
+    @State var isActive = false
+    
     var body: some View {
-        GeometryReader { proxy in
-            VStack {
-                Spacer()
-                    .frame(height: proxy.size.height * LandingConstants.percentTitle)
-                Text(LandingConstants.landingTitleText)
-                    .font(.system(size:LandingConstants.landingTitleSize, design: .rounded))
-                    .fontWeight(.medium)
-                    .foregroundColor(.white)
-                    .shadow(color: Color(.black), radius: 1, x: 2, y: 2)
-                Spacer()
-                Button(action: {
+        NavigationView {
+            GeometryReader { proxy in
+                VStack {
+                    Spacer()
+                        .frame(height: proxy.size.height * LandingConstants.percentTitle)
+                    Text(LandingConstants.landingTitleText)
+                        .font(.system(size:LandingConstants.landingTitleSize, design: .rounded))
+                        .fontWeight(.medium)
+                        .foregroundColor(.white)
+                        .shadow(color: Color(.black), radius: 1, x: 2, y: 2)
+                    Spacer()
                     
-                }) {
-                    HStack(spacing: 15) {
-                        Spacer()
-                        Image(systemName: LandingConstants.landingImageSystemButtonName)
-                            .font(.system (size: 24, weight: .semibold))
-                            .foregroundColor(Color(.white))
-                        Text(CommonsConstants.buttonText)
-                            .font(.system (size: 24))
-                            .foregroundColor(Color(.white))
-                        Spacer()
+                    NavigationLink(destination: Text("navigated"), isActive: self.$isActive) {
+                        Button(action: {
+                            self.isActive = true
+                        }) {
+                            HStack(spacing: 15) {
+                                Spacer()
+                                Image(systemName: LandingConstants.landingImageSystemButtonName)
+                                    .font(.system (size: 24, weight: .semibold))
+                                    .foregroundColor(Color(.white))
+                                Text(LandingConstants.landingButtonCreateNewIncreaseTitleText)
+                                    .font(.system (size: 24))
+                                    .foregroundColor(Color(.white))
+                                Spacer()
+                            }
+                        }
+                        .padding(.horizontal, 20)
+                        .buttonStyle(PrimaryButtonStyle())
                     }
                 }
-                .padding(.horizontal, 20)
-                .buttonStyle(PrimaryButtonStyle())
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(
+                    Image(LandingConstants.landingBackgroundImageName)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .overlay(Color.black.opacity(0.3))
+                        .frame(width: proxy.size.width)
+                        .edgesIgnoringSafeArea(.all)
+                )
             }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(
-                Image(LandingConstants.landingBackgroundImageName)
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .overlay(Color.black.opacity(0.3))
-                    .frame(width: proxy.size.width)
-                    .edgesIgnoringSafeArea(.all)
-            )
-        }
-        
+        }.accentColor(.primary)
     }
 }
 
