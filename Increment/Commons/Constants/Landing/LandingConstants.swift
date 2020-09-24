1ce9e9679cbe1f46424abb721ab664fbfed7fb5a
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-09-24 09:19:36 +0200

    [FEATURE][LANDING] * Landing UI was finished

--- Increment/Commons/Constants/Landing/LandingConstants.swift
+++ Increment/Commons/Constants/Landing/LandingConstants.swift
@@ -1,6 +1,12 @@
 
-import Foundation
+import SwiftUI
 
 struct LandingConstants {
     static let landingBackgroundImageName = "ropes"
+    static let landingTitleText = "Increment"
+    static let landingTitleSize: CGFloat = 64
+    static let percentTitle: CGFloat = 0.15
+    static let landingImageSystemButtonName = "plus.circle.fill"
+
+    
 }
--- Increment/Landing/LandingView.swift
+++ Increment/Landing/LandingView.swift
@@ -5,21 +5,39 @@ struct LandingView: View {
         GeometryReader { proxy in
             VStack {
                 Spacer()
-                    .frame(height: proxy.size.height * 0.25)
-                Text("Increment")
-                    .font(.system(size: 64, design: .rounded))
+                    .frame(height: proxy.size.height * LandingConstants.percentTitle)
+                Text(LandingConstants.landingTitleText)
+                    .font(.system(size:LandingConstants.landingTitleSize, design: .rounded))
                     .fontWeight(.medium)
                     .foregroundColor(.white)
                     .shadow(color: Color(.black), radius: 1, x: 2, y: 2)
                 Spacer()
+                Button(action: {
+                    
+                }) {
+                    HStack(spacing: 15) {
+                        Spacer()
+                        Image(systemName: LandingConstants.landingImageSystemButtonName)
+                            .font(.system (size: 24, weight: .semibold))
+                            .foregroundColor(Color(.white))
+                        Text(CommonsConstants.buttonText)
+                            .font(.system (size: 24))
+                            .foregroundColor(Color(.white))
+                        Spacer()
+                    }
+                }
+                .padding(.horizontal, 20)
+                .buttonStyle(PrimaryButtonStyle())
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .background(
                 Image(LandingConstants.landingBackgroundImageName)
                     .resizable()
                     .aspectRatio(contentMode: .fill)
+                    .overlay(Color.black.opacity(0.3))
+                    .frame(width: proxy.size.width)
+                    .edgesIgnoringSafeArea(.all)
             )
-                .edgesIgnoringSafeArea(.all)
         }
         
     }
